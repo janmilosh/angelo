8a5b9dca1a156649df92bfe791f3f48324c9e011
Jan Milosh <janmilosh@gmail.com>
2014-02-18 19:44:56 -0500

    Adds select control

---
+++ README.md
@@ -0,0 +1,3 @@
+*Angelo
+
+An angular practice app based on the book *AngularJS in Action* by Brian Ford and Lukas Ruebbelke.
--- js/app.js
+++ js/app.js
@@ -10,16 +10,39 @@ myModule.controller('MainCtrl', function($scope) {
 	$scope.createStory = function(currentStory) {
 		$scope.stories.push({
 			title: 'New Story',
-			description: 'Description pending.'
+			description: 'Description pending.',
+			criteria: 'Criteria pending',
+			status: 'Status pending',
+			type: 'Type pending',
+			reporter: 'Reporter pending',
+			assignee: 'assignee pending'
 		});
 	};
 
-	$scope.stories = [
-		{title: 'Story 00', description: 'Description pending.'},
-		{title: 'Story 01', description: 'Description pending.'},
-		{title: 'Story 02', description: 'Description pending.'},
-		{title: 'Story 03', description: 'Description pending.'},
-		{title: 'Story 04', description: 'Description pending.'},
-		{title: 'Story 05', description: 'Description pending.'}
-		];
+	$scope.stories = [{
+		title: 'Story 00',
+		description: 'Description pending.',
+		criteria: 'Criteria pending',
+		status: 'To Do',
+		type: 'Feature',
+		reporter: 'Jan Milosh',
+		assignee: 'Jan Milosh'
+	}];
+
+	$scope.statuses = [
+		{name: 'Back Log'},
+		{name: 'To Do'},
+		{name: 'In Progress'},
+		{name: 'Code Review'},
+		{name: 'QA Review'},
+		{name: 'Verified'},
+		{name: 'Done'}
+	];
+
+	$scope.types = [
+		{name: 'Feature'},
+		{name: 'Enhancement'},
+		{name: 'Bug'},
+		{name: 'Spike'}
+	];
 });
